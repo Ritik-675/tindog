bc101d4e0208e35d54f5e6867e8e0a0dcbd69c1c
ritik675 <ritik.yadav.ug20@nsut.ac.in>
2021-11-07 18:18:41 +0530

    done

---
+++ login.js
@@ -0,0 +1,13 @@
+var user = document.querySelector("#userName") ;
+var pass = document.querySelector("#userPassword") ;
+var butt = document.querySelector(".button-submit") ;
+var out = document.querySelector(".out-display") ;
+function loginPage(){
+    console.log("sjs") ;
+    if(user.value==="ritik" && pass.value==="ritik"){
+        window.open("https://tindog-ritz.netlify.app/","_self");
+    }else{
+        out.innerHTML = "**Incorrect Credentials**" ;
+    }
+}
+butt.addEventListener("click",loginPage) ;
